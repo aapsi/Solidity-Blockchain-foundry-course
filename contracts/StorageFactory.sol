70247825933551b86cb43a146efeffd0eeb0179f
aapsi <aapsi0001@gmail.com>
2023-06-30 12:43:59 +0530

    abi

--- contracts/StorageFactory.sol
+++ contracts/StorageFactory.sol
@@ -1,15 +1,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity  ^0.8.18;
 
-import {SimpleStorage} from "./SimpleStorage.sol";
+import "./SimpleStorage.sol";
 
 contract StorageFactory {
 
     //uint256 public favouriteNumber
     //type visibility name
-    SimpleStorage public simpleStorage;
+    SimpleStorage[] public listOfSimpleStorageContracts;
     
     function createSimpleStorageContract() public {
-        simpleStorage = new SimpleStorage();
+        SimpleStorage newSimpleStorageContract = new SimpleStorage();
+        listOfSimpleStorageContracts.push(newSimpleStorageContract);
+    }
+
+    function sfStore(uint256 _simpleStorageIndex, uint256 _newSimpleStorageNumber) public {
+        //Address
+        //ABI- Application Binary Interface
+        SimpleStorage mySimpleStorage = listOfSimpleStorageContracts[_simpleStorageIndex];
+        mySimpleStorage.store(_newSimpleStorageNumber);
+    }
+
+    function sfGet(uint256 _simpleStorageIndex) public view returns(uint256) {
+        SimpleStorage mySimpleStorage = listOfSimpleStorageContracts[_simpleStorageIndex];
+        return mySimpleStorage.retrieve();
     }
 }
