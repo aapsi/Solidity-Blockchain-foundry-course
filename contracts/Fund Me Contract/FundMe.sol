37e178c9806febc830941b3a53b209d94fd45ce1
aapsi <aapsi0001@gmail.com>
2023-07-04 02:50:58 +0530

    receiving

--- contracts/Fund Me Contract/FundMe.sol
+++ contracts/Fund Me Contract/FundMe.sol
@@ -1,14 +1,42 @@
   // SPDX-License-Identifier: MIT
- pragma solidity ^0.8.18;
+pragma solidity ^0.8.18;
 
- contract FundMe {
-     function fund() public payable {
-         //allow users to send money
-         // have a minimum sending amount
-         require(msg.value > 1e18, "didn't send enough eth"); // 1e18 = 1 eth
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import {PriceConverter} from "./PriceConverter.sol";
 
-        // What is revert?
-        // undo any actions that have been done, and send remaining gas back
-        // chainlink oracles 
-     }
- }
+contract FundMe {
+    using PriceConverter for uint256;
+    uint256 public minimumUSd = 5e18;
+
+    address[] public funders;
+    mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
+
+    function fund() public payable {
+        // we did not pass any value as msg.value is of uint256 type and it gets passed already
+
+        //allow users to send money
+        // have a minimum sending amount
+        require(msg.value.getConversionRate() > minimumUSd, "didn't send enough eth"); // 1e18 = 1 eth
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
+    // What is revert?
+    // undo any actions that have been done, and send remaining gas back
+    // chainlink oracles 
+    }
+
+    function withdraw() public {
+        for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        funders = new address[](0);
+        //resetting the funders array to a new empty array starting at zero
+
+        //transfer
+        
+        //send
+        // call
+    }
+
+
+    }
---
+++ contracts/Fund Me Contract/PriceConverter.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+library PriceConverter {
+    function getPrice() internal view returns(uint256) {
+    // Address 0x694AA1769357215DE4FAC081bf1f309aDC325306
+    // address of the contract that does eth/usd conversion on chainlink
+    // ABI
+
+    AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+    (, int256 price, , , )= priceFeed.latestRoundData();
+    // price of eth in terms of usd
+    // 2000.00000000
+    return uint256(price * 1e10);
+    }
+
+    function getConversionRate(uint256 ethAmount) internal view returns(uint256){
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+        return ethAmountInUsd;
+
+    }
+
+    function getVersion() internal view returns (uint256) {
+        return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
+        // using interface
+    }
+
+}
